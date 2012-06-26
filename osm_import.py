fffd3280e673e490572b5ed4f9b3e057306988d1
David King <dave@davbo.org>
2012-06-26 15:40:10 +0100

    Initial commit working import for OSM data into ES

---
+++ osm_import.py
@@ -0,0 +1,90 @@
+import bz2  # TODO: Import using a bzip compressed file
+import logging
+
+from xml.sax import handler, make_parser
+
+from pyes import ES, TermQuery
+
+h = logging.StreamHandler()
+logger = logging.getLogger(__name__)
+logger.addHandler(h)
+INDEX_NAME = "places"
+MAPPING_NAME = "base_places"
+
+
+class OSMHandler(handler.ContentHandler):
+
+    def __init__(self, mapping):
+        self.conn = ES('http://127.0.0.1:9200')
+        self.conn.create_index(INDEX_NAME)
+        self.conn.put_mapping(MAPPING_NAME, {'properties': mapping}, [INDEX_NAME])
+
+    def startDocument(self):
+        self.tags = {}
+        self.valid_node = True
+        self.create_count, self.modify_count = 0, 0
+        self.delete_count, self.unchanged_count = 0, 0
+        self.ignore_count = 0
+        self.node_locations = {}
+
+    def startElement(self, name, attrs):
+        if name == 'node':
+            lon, lat = float(attrs['lon']), float(attrs['lat'])
+            id = attrs['id']
+            self.node_location = lon, lat
+            self.attrs = attrs
+            self.id = id
+            self.tags = {}
+            self.node_locations[id] = lon, lat
+        elif name == 'tag':
+            self.tags[attrs['k']] = attrs['v']
+        elif name == 'way':
+            self.nodes = []
+            self.tags = {}
+            self.attrs = attrs
+            self.id = attrs['id']
+        elif name == 'nd':
+            self.nodes.append(attrs['ref'])
+
+    def endElement(self, element_type):
+        if element_type == 'node':
+            location = self.node_location
+        elif element_type == 'way':
+            min_, max_ = (float('inf'), float('inf')), (float('-inf'), float('-inf'))
+            for lon, lat in [self.node_locations[n] for n in self.nodes]:
+                min_ = min(min_[0], lon), min(min_[1], lat)
+                max_ = max(max_[0], lon), max(max_[1], lat)
+            location = (min_[0] + max_[0]) / 2, (min_[1] + max_[1]) / 2
+        if element_type in ['way', 'node'] and 'amenity' in self.tags:
+            result = dict([('osm:%s' % k, v) for k, v in self.tags.items()])
+            result['osm:type'] = element_type
+            result['osm:version'] = self.attrs['version']
+            result['identifiers'] = ['osm:%s' % self.id]
+            # Some ameneties do not have names, this is correct behaviour.
+            # For example, post boxes and car parks.
+            result['name'] = self.tags.get('name', self.tags.get('operator', None))
+            result['location'] = location
+            self.conn.index(result, INDEX_NAME, MAPPING_NAME)
+
+if __name__ == '__main__':
+    mapping = {
+            'identifiers': {
+                'type': 'string',
+                'store': 'yes',
+                'index': 'not_analyzed',
+                'index_name': 'identifier',
+                },
+            'location': {
+                'type': 'geo_point',
+                }
+            }
+    parser = make_parser(['xml.sax.xmlreader.IncrementalParser'])
+    parser.setContentHandler(OSMHandler(mapping))
+    # Parse in 8k chunks
+    osm = open('oxfordshire.osm')
+    buffer = osm.read(8192)
+    # bunzip = bz2.BZ2Decompressor()
+    while buffer:
+        parser.feed(buffer)
+        buffer = osm.read(8192)
+    parser.close()
